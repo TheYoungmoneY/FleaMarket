ff68ba404de3ebdd6d4e03feb6eb1fa31f3b801d
Youngmi <youngmi9347@gmail.com>
2020-04-08 22:38:18 +0900

    ClassCounter

---
+++ ClassCounter/src/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Styled from 'styled-components/native';
+import Counter from './Screens/Counter';
+
+
+const Containner = Styled.View`
+    flex: 1;
+    background-color : #EEE;
+`;
+
+
+const App = ({ }: Props) => {
+  return (
+    <Containner>
+      <Counter title="This is a Counter App" initValue={5} />
+    </Containner>
+  );
+};
+
+export default App;
